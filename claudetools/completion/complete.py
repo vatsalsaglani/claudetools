89b0a25ef52ea101625b04b26e3979f1334f8922
vatsalsaglani <saglanivatsal@gmail.com>
2024-03-20 19:38:23 +0530

    formatting update

--- claudetools/completion/complete.py
+++ claudetools/completion/complete.py
@@ -15,12 +15,12 @@ class Complete:
         else:
 
             if aws_session_token:
-                self.client = AnthropicBedrock(aws_session_token=aws_session_token,
-                                        aws_region=aws_region)
+                self.client = AnthropicBedrock(
+                    aws_session_token=aws_session_token, aws_region=aws_region)
             else:
                 self.client = AnthropicBedrock(aws_access_key=aws_access_key,
-                                        aws_secret_key=aws_secret_key,
-                                        aws_region=aws_region)
+                                               aws_secret_key=aws_secret_key,
+                                               aws_region=aws_region)
 
     def __call__(self, model: str, messages: List[Dict], **kwargs):
         output = self.client.messages.create(model=model,
