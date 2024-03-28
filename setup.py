e5d05b79a15082e6fac3c626daddeeb164351b31
vatsalsaglani <saglanivatsal@gmail.com>
2024-03-28 10:59:55 +0530

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="claudetools",
-    version="0.7.1",
+    version="0.8.0",
     author="Vatsal J. Saglani",
     author_email="saglanivatsal@gmail.com",
     description=
